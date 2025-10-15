07131d264e159c4f3f9318d58ea8c04d50414da3
grek2 <madisonwharton0@gmail.com>
2025-10-15 15:20:23 -0400

    aaaa

--- main3.py
+++ main3.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
 size = 50
 speed = 50
+start = " "
 
 pygame.init()
 pygame.display.set_caption("Cellular Tomato :)")
@@ -14,13 +16,19 @@ running = True
 def getNextStep(lifeGrid):
     a, b = size, size
 
-    #can have 4 or 8 neighbors
+    #can have 4 or 8 neighbors. 
     directions8 = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
     directions4 = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    directionsUp = [(0, 1), (1, 1), (-1, 1),(0, -1)]
 
     #rules for cellular automata.
-    bornNbrs = [3]
+    
+    bornNbrs = [2]
     surviveNbrs = [2, 3]
+    '''
+    bornNbrs = [3, 6, 7, 8]
+    surviveNbrs = [3, 4, 6, 7, 8]
+    '''
     directions = directions8
 
     for i in range(a):
@@ -30,29 +38,29 @@ def getNextStep(lifeGrid):
             for dirX, dirY in directions:
                 (x, y) = (i + dirX, j + dirY)
 
-                if 0 <= x < a and 0 <= y < b and (lifeGrid[x][y] == 1 or lifeGrid[x][y] == -1):
+                if 0 <= x < a and 0 <= y < b and (lifeGrid[x][y] == 1 or lifeGrid[x][y] == "dying"):
                     liveNbrs += 1
 
 
             if lifeGrid[i][j] == 0 and liveNbrs in bornNbrs:
-                lifeGrid[i][j] = 2
+                lifeGrid[i][j] = "growing"
 
             elif (lifeGrid[i][j] == 1) and not(liveNbrs in surviveNbrs):
-                lifeGrid[i][j] = -1
+                lifeGrid[i][j] = "dying"
                 
             
     for i in range(a):
         for j in range(b):            
-            if lifeGrid[i][j] == 2:
+            if lifeGrid[i][j] == "growing":
                 #pygame.draw.rect(screen, pygame.Color(0, 0, 255, 255), ((j)*boxSize, (i)*boxSize, boxSize, boxSize), border_radius=0)
                 lifeGrid[i][j] = 1
 
-            elif lifeGrid[i][j] == -1:
-                pygame.draw.rect(screen, pygame.Color(255, 0, 0, 255), ((j)*boxSize, (i)*boxSize, boxSize, boxSize), border_radius=0)
+            elif lifeGrid[i][j] == "dying":
+                #pygame.draw.rect(screen, pygame.Color(255, 0, 0, 255), ((i)*boxSize, (j)*boxSize, boxSize, boxSize), border_radius=0)
                 lifeGrid[i][j] = 0
             #else:
                 #pygame.draw.rect(screen, pygame.Color(0, 255*int(lifeGrid[i][j]), 0, 255), ((j)*boxSize, (i)*boxSize, boxSize, boxSize), border_radius=0)        
-            pygame.draw.rect(screen, pygame.Color(5, 255*int(lifeGrid[i][j]), 5, 255), ((j)*boxSize, (i)*boxSize, boxSize, boxSize), border_radius=0)
+            pygame.draw.rect(screen, pygame.Color(5, 255*int(lifeGrid[i][j]), 5, 255), ((i)*boxSize, (j)*boxSize, boxSize, boxSize), border_radius=0)
             #print(lifeGrid[i][j], end=" ")
         #print()
 
@@ -61,17 +69,20 @@ lifeGrid = []
 for c in range(size):
     die = []
     for d in range(size):
-        die.append(0)
+        if start == "random":
+            die.append(random.randint(0,1))
+        else:
+            die.append(0)
     lifeGrid.append(die)
 
 
 #lifeGrid[1][1] = 1
 
-lifeGrid[2][2] = 1
-lifeGrid[3][3] = 1
-lifeGrid[3][4] = 1
-lifeGrid[2][4] = 1
-lifeGrid[1][4] = 1 
+lifeGrid[42][42] = 1
+lifeGrid[43][43] = 1
+lifeGrid[43][44] = 1
+lifeGrid[42][44] = 1
+lifeGrid[41][44] = 1 
 
 
 while running:
